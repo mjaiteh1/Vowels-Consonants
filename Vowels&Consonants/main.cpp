8e63af8486108f4da0fede3699d22dd82d6dbe3c
Mariama Jaiteh <mjaiteh@smith.edu>
2016-10-19 17:18:35 -0400

    Initial Commit

---
+++ Vowels&Consonants/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Vowels&Consonants
+//
+//  Created by Mariama Jaiteh on 10/19/16.
+//  Copyright © 2016 Mariama Jaiteh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
