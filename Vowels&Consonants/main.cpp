9bef21e76a1756dc4fb63dbc76dbc0b6e51833d6
Mariama Jaiteh <mjaiteh@smith.edu>
2016-10-23 10:19:23 -0400

    vowels&consonants

--- Vowels&Consonants/main.cpp
+++ Vowels&Consonants/main.cpp
@@ -1,15 +1,47 @@
 //
 //  main.cpp
 //  Vowels&Consonants
-//
+// Determine how many of the characters are vowels and how many are consonants in a given line of text.
 //  Created by Mariama Jaiteh on 10/19/16.
 //  Copyright © 2016 Mariama Jaiteh. All rights reserved.
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    char tmp;
+    int vowelCounter = 0;
+    int consonantCounter = 0;
+    string word = "";
+    cout << "Word: ";
+    getline(cin,word);
+    
+
+    
+    for (int i = 0; i < word.size(); i++)
+    {
+        tmp = toupper(word[i]);
+        switch(tmp)
+        {
+            case 'A' :
+            case 'E' :
+            case 'I' :
+            case 'O' :
+            case 'U' :
+                vowelCounter++;
+                break;
+            case ' ':
+                continue;
+            default:
+                consonantCounter++;
+        }
+    }	
+    cout << "Total vowels = " << vowelCounter << endl;
+    cout << "Total consonants = " << consonantCounter << endl;		
+    
     return 0;
+    
 }
